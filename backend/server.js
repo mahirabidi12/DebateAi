42d961428871e67824196eda0eaaa0a447542467
Mahir Abidi <abidimahir109@gmail.com>
2025-09-13 18:25:58 +0530

    Database Setup

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 import express from 'express'
 import app from './app.js'
 import dotenv from 'dotenv'
+import connectDb from './config/db.js'
 
 dotenv.config()
 
 app.listen(process.env.PORT , () => {
     console.log(`Server is running on port ${process.env.PORT}`)
 })
+
+connectDb()
