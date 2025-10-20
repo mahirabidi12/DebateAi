a76b11f83813a10f31bd7dcfe4eef23e5fec7318
Mahir Abidi <abidimahir109@gmail.com>
2025-10-20 19:13:12 +0530

    Backend Auth Update

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -37,6 +37,10 @@ const userSchema = mongoose.Schema({
     type: Number,
     default: 0,
   },
+  debateHeadings: {
+    type: [String],
+    default: [],
+  },
 });
 
 
--- backend/server.js
+++ backend/server.js
@@ -4,12 +4,19 @@ import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
 import connectDb from './config/db.js'
 import authRouter from './routes/authRoutes.js'
+import cors from "cors";
 
 dotenv.config()
 
 app.use(express.json())
 app.use(express.urlencoded({extended : true}))
 app.use(cookieParser())
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL, // your frontend URL
+    credentials: true, // if using cookies or authorization headers
+  })
+);
 
 app.listen(process.env.PORT , () => {
     console.log(`Server is running on port ${process.env.PORT}`)
