79cf54044ddfc797964904b37ba95f2afa4cd9e5
Mahir Abidi <abidimahir109@gmail.com>
2025-10-20 22:49:20 +0530

    GeminiResponse Setup

---
+++ backend/services/gemini.js
@@ -0,0 +1,19 @@
+import { GoogleGenAI } from "@google/genai";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const ai = new GoogleGenAI({});
+
+export async function getGeminiResponse(prompt) {
+  try {
+    const response = await ai.models.generateContent({
+      model: "gemini-2.5-flash",
+      contents: prompt,
+    });
+    console.log(response.text);
+  } catch (error) {
+    throw new Error("Failed to generate content from Gemini");
+  }
+}
+
